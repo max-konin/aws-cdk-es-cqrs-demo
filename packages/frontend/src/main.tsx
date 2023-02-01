fa5a0781b839222acec41c3f508c0dc8d1bbde26
Max Konin <maxim21214@gmail.com>
2023-02-01 11:11:16 -0300

    Share API keys between back and UI

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,14 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Amplify } from 'aws-amplify';
 import App from './App';
 import './index.css';
+import Params from '../../backend/cdk-outputs.json';
 
 Amplify.configure({
-  aws_appsync_region: 'us-west-2', // Stack region
-  aws_appsync_graphqlEndpoint: 'https://2fwncb6jtnhn5p6ilq7ybe7c6m.appsync-api.us-east-1.amazonaws.com/graphql', // AWS AppSync endpoint
+  aws_appsync_region: Params.BackendStack.StackRegion,
+  aws_appsync_graphqlEndpoint: Params.BackendStack.GraphQLAPIURL,
   aws_appsync_authenticationType: 'API_KEY', //Primary AWS AppSync authentication type
-  aws_appsync_apiKey: 'da2-vdn7gfqrundorfkn3wuz4elnvy', // AppSync API Key
+  aws_appsync_apiKey: Params.BackendStack.GraphQLAPIKey,
 });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
