af74fb96d4819d5a09cd1d3235a18c03978ed50c
Max Konin <maxim21214@gmail.com>
2023-02-02 10:58:22 -0300

    Add Cognito to backend stack

--- packages/backend/lib/backend-stack.ts
+++ packages/backend/lib/backend-stack.ts
@@ -9,10 +9,94 @@ import { Construct } from 'constructs';
 import { AccountsConstruct } from './accounts/accounts-construct';
 import { aws_dynamodb } from 'aws-cdk-lib';
 import { StreamViewType } from 'aws-cdk-lib/aws-dynamodb';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const userPool = new cognito.UserPool(this, 'userpool', {
+      userPoolName: 'my-user-pool',
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      standardAttributes: {
+        givenName: {
+          required: true,
+          mutable: true,
+        },
+        familyName: {
+          required: true,
+          mutable: true,
+        },
+      },
+      customAttributes: {
+        tenantId: new cognito.StringAttribute({ mutable: false }),
+      },
+      passwordPolicy: {
+        minLength: 6,
+        requireLowercase: true,
+        requireDigits: true,
+        requireUppercase: false,
+        requireSymbols: false,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    // 👇 User Pool Client attributes
+    const standardCognitoAttributes = {
+      givenName: true,
+      familyName: true,
+      email: true,
+      emailVerified: true,
+      address: true,
+      birthdate: true,
+      gender: true,
+      locale: true,
+      middleName: true,
+      fullname: true,
+      nickname: true,
+      phoneNumber: true,
+      phoneNumberVerified: true,
+      profilePicture: true,
+      preferredUsername: true,
+      profilePage: true,
+      timezone: true,
+      lastUpdateTime: true,
+      website: true,
+    };
+
+    const clientReadAttributes =
+      new cognito.ClientAttributes().withStandardAttributes(
+        standardCognitoAttributes
+      );
+
+    const clientWriteAttributes =
+      new cognito.ClientAttributes().withStandardAttributes({
+        ...standardCognitoAttributes,
+        emailVerified: false,
+        phoneNumberVerified: false,
+      });
+
+    // 👇 User Pool Client
+    const userPoolClient = new cognito.UserPoolClient(this, 'userpool-client', {
+      userPool,
+      authFlows: {
+        adminUserPassword: true,
+        custom: true,
+        userSrp: true,
+      },
+      supportedIdentityProviders: [
+        cognito.UserPoolClientIdentityProvider.COGNITO,
+      ],
+      readAttributes: clientReadAttributes,
+      writeAttributes: clientWriteAttributes,
+    });
 
     const eventStore = new aws_dynamodb.Table(this, 'EventStore', {
       partitionKey: {
@@ -73,6 +157,15 @@ export class BackendStack extends cdk.Stack {
     // Prints out the stack region to the terminal
     new cdk.CfnOutput(this, 'Stack Region', {
       value: this.region,
+    });
+
+    // Prints Cognito Pool Id
+    new cdk.CfnOutput(this, 'UserPullId', {
+      value: userPool.userPoolId,
+    });
+
+    new cdk.CfnOutput(this, 'UserPoolClientId', {
+      value: userPoolClient.userPoolClientId,
     });
 
     const accountsMutationsDS = api.addLambdaDataSource(
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import App from './App';
 import './index.css';
 import Params from '../../backend/cdk-outputs.json';
@@ -12,7 +12,14 @@ Amplify.configure({
   aws_appsync_graphqlEndpoint: Params.BackendStack.GraphQLAPIURL,
   aws_appsync_authenticationType: 'API_KEY', //Primary AWS AppSync authentication type
   aws_appsync_apiKey: Params.BackendStack.GraphQLAPIKey,
+  Auth: {
+    userPoolId: Params.BackendStack.UserPullId,
+    authenticationFlowType: 'USER_PASSWORD_AUTH',
+    userPoolWebClientId: Params.BackendStack.UserPoolClientId,
+  },
 });
+
+Auth.configure();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
